676b458609eda8db7d9b95d7954580d7befb38da
GitDeib <eltondavedeono2004@gmail.com>
2025-04-25 05:45:10 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using IT15_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IT15_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Ride()
+        {
+            return View();
+        }
+        public IActionResult Rental()
+        {
+            return View();
+        }
+        public IActionResult DriverReg()
+        {
+            return View();
+        }
+        public IActionResult DriverIntro()
+        {
+            return View();
+        }
+        public IActionResult RentOwner()
+        {
+            return View();
+        }
+        public IActionResult RentIntro()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RideBookingModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+public class RideBookingModel : PageModel
+{
+    [BindProperty]
+    public RideBookingInputModel Input { get; set; }
+
+    public void OnGet() { }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        // TODO: Save booking to DB, redirect to success page, etc.
+        return RedirectToPage("/RideConfirmation");
+    }
+}
+
+public class RideBookingInputModel
+{
+    [Required]
+    public string PickupLocation { get; set; }
+
+    [Required]
+    public string DropOffLocation { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime RideDate { get; set; }
+
+    [Required]
+    [DataType(DataType.Time)]
+    public TimeSpan RideTime { get; set; }
+
+    [Required]
+    public string RideType { get; set; }
+
+    public string Notes { get; set; }
+}
